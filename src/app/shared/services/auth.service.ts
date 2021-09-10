21ae7b4799e018297e1db108290aaa7323b17fc1
Noa <noaisraeli3@gmail.com>
2021-09-10 19:19:48 +0300

    added auth signup - not checked yet

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, NgZone } from '@angular/core';
-import { UserData } from "../services/user";
+import { Tourist, UserData, Guide } from "../services/user";
 import firebase, { auth, User } from 'firebase/app';
 import { AngularFireAuth } from "@angular/fire/auth";
 import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
@@ -38,29 +38,90 @@ export class AuthService {
   }
 
   // Sign in with email/password
-  SignIn(email: string, password: string) {
-    return this.afAuth.signInWithEmailAndPassword(email, password)
-      .then((result) => {
-        this.ngZone.run(() => {
-          this.router.navigate(['dashboard']);
-        });
-        this.SetUserData(result.user);
+  SignIn(email: string, password: string): void {
+    console.log(email, password);
+    this.afAuth.signInWithEmailAndPassword(email, password)
+      .then(res => {
+        console.log(res);
+        this._userData = this.afAuth.authState;
+        this.afs.collection<UserData>('users')
+          .doc<UserData>(res.user.uid)
+          .valueChanges()
+          .subscribe((user) => {
+            console.log(user);
+            this.currentUser = user;
+            this.currentUser$.next(this.currentUser);
+            if( ! firebase.auth().currentUser.emailVerified){
+              window.alert("Please Verify Your Email Account");
+              this.router.navigate(['sign-in']);
+            }
+            else{
+              this.router.navigate(['dashboard']);
+            }
+          });
       }).catch((error) => {
-        window.alert(error.message)
-      })
+        console.log(error.message);
+        window.alert(error);
+      });
   }
 
   // Sign up with email/password
-  SignUp(email: string, password: string) {
-    return this.afAuth.createUserWithEmailAndPassword(email, password)
-      .then((result) => {
-        /* Call the SendVerificaitonMail() function when new user sign
-        up and returns promise */
-        this.SendVerificationMail();
-        this.SetUserData(result.user);
-      }).catch((error) => {
-        window.alert(error.message)
+  SignUp( userData: UserData, password: string ) {
+    console.log(userData.email, password);
+
+    this.afAuth.createUserWithEmailAndPassword(userData.email, password)
+      .then(res => {
+        if (res) {
+          this.afs.collection('users').doc(res.user.uid)
+            .set({
+              email: userData.email,
+              firstName: userData.firstName,
+              lastName: userData.lastName,
+              phoneNumber: userData.phoneNumber,
+              tourist: userData.tourist,
+              guide: userData.guide
+            }).then(value => {
+              this.afs.collection<UserData>('users')
+                .doc<UserData>(res.user.uid)
+                .valueChanges()
+                .subscribe(user => {
+                  console.log(user);
+                  if (user) {
+                    this.currentUser$.next(user);
+                  }
+                  if ( user.tourist ) {
+                    this.afs.collection('tourists').doc(user.uid)
+                    .set({
+                      userData: user,
+                      tourismType: (user.specialData as Tourist).tourismType,
+                      groupType: (user.specialData as Tourist).groupType,
+                      language: (user.specialData as Tourist).language,
+                      hasCar: user.specialData.hasCar
+                    })
+                  }else if ( user.guide ) {
+                    this.afs.collection('guides').doc(user.uid)
+                    .set({
+                      userData: user,
+                      age: (user.specialData as Guide).age,
+                      tourismTypes: (user.specialData as Guide).tourismTypes,
+                      languages: (user.specialData as Guide).languages,
+                      hasCar: (user.specialData as Guide).hasCar,
+                    })
+                    
+                  }
+                  this.SendVerificationMail();
+                });
+            });
+        }
       })
+      .catch((error) => {
+        console.log(`Something went wrong ${error.message}`);
+        window.alert(error);
+      })
+  }
+
+  get userData(): Observable<firebase.User> {
+    return this._userData;
   }
 
   // Send email verfificaiton when new user sign up
